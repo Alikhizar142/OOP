9c1ef5638d5f7c37ba46a2482266d468e832d2ab
Khizar Ali <111997224+Alikhizar142@users.noreply.github.com>
2023-02-28 16:12:09 +0500

    Delete Bookstore.cpp

--- Classes/Bookstore.cpp
+++
@@ -1,6 +0,0 @@
-/*Using the classes designed in Programming Exercises 6 and 7, write a program to simulate a bookstore. 
-The bookstore has two types of customers: those who are members of the bookstore and those who buy books from the bookstore only occasionally. 
-Each member has to pay a $10 yearly membership fee and receives a 5% discount on each book purchased. For each member, the bookstore keeps track of the number of books purchased and the total amount spent. 
-For every eleventh book that a member buys, the bookstore takes the average of the total amount of the last 10 books purchased, applies this amount as a discount, and then resets the total amount spent to 0.
-Write a program that can process up to 1000 book titles and 500 members. Your program should contain a menu that gives the user different choices to effectively run the program; in other words, your program should be user driven.
-*/

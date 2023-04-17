2e0112f71f752a42794d4fa4e2164f8e99ced214
Khizar Ali <111997224+Alikhizar142@users.noreply.github.com>
2023-04-17 10:52:24 +0500

    Create Zoo updated polii.cpp

---
+++ Zoo updated polii.cpp
@@ -0,0 +1,130 @@
+/*You are required to design a program for a zoo where there are different types and categories 
+of animals. Use Inheritance and polymorphism to write a maintainable code. 
+Animal is an abstract class with two protected data members name and color, destructor, and a 
+pure virtual function display().
+Deer is derived from Animal and has an additional boolean data member hasAntlers, indicating 
+whether or not the deer has antlers. Deer overrides the display() function to print information 
+about the deer's name, color, and whether or not it has antlers.
+Zebra is derived from Animal and has an additional string data member stripe_pattern, 
+indicating the pattern of stripes on the zebra's body. Zebra overrides the display() function to 
+print information about the zebra's name, color, and stripe pattern.
+In main, using polymorphism create child’s class objects and call their display() methods.
+Task 02
+Now make changes to existing code in a way that in the main, create an array of pointers to the 
+Animal class and populate it with objects of different derived classes. Pass this array to a 
+function named displayAll(). In that function call the display() method with each array element.
+Finally, in main, at the end destroy the objects and release the memory. Make sure that the 
+child’s class objects should be destroyed*/
+
+
+#include<iostream>
+using namespace std;
+class Animal
+{   protected:
+    string name;
+    string color;
+    public:
+    Animal(string n="",string c="");
+    virtual void display();
+    virtual ~Animal();
+};
+Animal::Animal(string n,string c)
+{
+    name=n;
+    color=c;
+}
+void Animal::display()
+{
+
+}
+Animal::~Animal()
+{
+
+}
+class Deer:public Animal
+{
+    private:
+    bool Antlers;
+    public:
+    Deer(string n="",string c="",bool a=0);
+    void display();
+};
+Deer::Deer(string n,string c,bool a):Animal(n,c)
+{
+    Antlers=a;
+}
+void Deer::display()
+{
+    cout<<"Im a deer my name is "<<name<<endl;
+    cout<<"Im a deer my color is "<<color<<endl;
+    if(Antlers)
+    {
+        cout<<"Im a deer i have antlers."<<endl;
+    }
+    else
+    {
+        cout<<"Im a deer i don't have antlers."<<endl;
+    }
+}
+
+class Zebra : public Animal
+{
+    string strip;
+    public:
+    Zebra(string n="",string c="",string s="");
+    void display();
+};
+Zebra::Zebra(string n,string c,string s):Animal(n,c)
+{
+    strip=s;
+}
+void Zebra::display()
+{
+    cout<<"Im a zebra my name is "<<name<<endl;
+    cout<<"Im a zebra my color is "<<color<<endl;
+    cout<<"Im a zebra i have "<<strip<<" strip."<<endl;
+}
+class Elephent : public Animal
+{
+    int weight;
+    public:
+    Elephent(string s="",string c="",int w=0);
+    void display();
+};
+
+Elephent::Elephent(string s,string c,int w):Animal(s,c)
+{
+    weight=w;
+}
+void Elephent:: display()
+{
+    cout<<"Im a Elephant my name is "<<name<<endl;
+    cout<<"Im a Elephant my color is "<<color<<endl;
+    cout<<"Im a Elephant my weight is"<<weight<<endl;
+
+}
+
+
+
+void displayall(Animal *a[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        a[i]->display();
+    }
+}
+int main()
+{   int n;
+    cout<<"Enter size of array"<<endl;
+    cin>>n;
+    Animal *A[n];
+    A[0]=new Deer("Deer","White",true);
+    A[1]=new Zebra("Zebra","Black","striped");
+    A[2]=new Elephent("Elephant","Grey",220);
+    displayall(A,n);
+    for(int i=0;i<n;i++)
+    {
+       delete A[i];
+    }
+    return 0;
+}
